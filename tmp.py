16f718434757efa538f3e49499b7492a574b3939
Diobrandokill <32999878+Diobrandokill@users.noreply.github.com>
2020-10-30 21:28:46 +0800

    leetcode 10.30

--- tmp.py
+++ tmp.py
@@ -1,13 +1,9 @@
 class Solution:
-    visited = set()
-    m,n,k =0,0,0
-    def dfs(self,i,j,si,sj):
-        if i >= self.m or j >= self.n or self.k < si + sj or (i,j) in self.visited: return 0
-        self.visited.add((i,j))
-        return 1 + self.dfs(i+1,j,si+1 if (i+1) % 10 else si-8,sj) + self.dfs(i,j+1,si,sj+1 if (j+1) % 10 else sj-8)
-    def movingCount(self, m: int, n: int, k: int) -> int:
-        self.m,self.n,self.k = m,n,k
-        return self.dfs(0,0,0,0)
+    def cuttingRope(self, n: int) -> int:
+        i = n // 3
+        j = n % 3
+        res = max(pow(3,i),pow(2,k)
+        return res * j if i != 0 else 1
 
 sol = Solution()
-print(sol.movingCount(3,2,17))
+print(sol.cuttingRope(10))
