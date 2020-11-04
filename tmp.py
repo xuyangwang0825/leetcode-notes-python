7fb57026c8d8e1ff3d5502ad9b98279d7084238a
Diobrandokill <32999878+Diobrandokill@users.noreply.github.com>
2020-11-04 22:10:15 +0800

    leetcode11.4

--- tmp.py
+++ tmp.py
@@ -1,11 +1,23 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        res = 0
-        while n > 1:
-            res += n % 2
+    def myPow(self, x: float, n: int) -> float:
+        res = 1
+        if n == 0: return res
+        if n < 0: x, n = 1/x, -n
+        while n:
+            if n % 2: res *= x
+            x *= x
             n //= 2
-        res += 1
+        return res
+    def myPow2(self, x: float, n: int) -> float:
+        if x == 0: return 0
+        res = 1
+        if n < 0: x, n = 1 / x, -n
+        while n:
+            if n & 1: res *= x
+            x *= x
+            n >>= 1
         return res
 
 sol = Solution()
-print(sol.hammingWeight(11))
+print(sol.myPow(2.00000,10))
+print(sol.myPow2(2.00000,10))
