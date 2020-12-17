2ac733f0ecdd78d8b46b42618070100ba1fa7d2e
Diobrandokill <32999878+Diobrandokill@users.noreply.github.com>
2020-12-17 20:11:55 +0800

    leetcode2020.12.17

--- tmp.py
+++ tmp.py
@@ -1,22 +1,13 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        res = 1
-        if n == 0: return res
-        if n < 0: x, n = 1/x, -n
-        while n:
-            if n % 2: res *= x
-            x *= x
-            n //= 2
-        return res
-    def myPow2(self, x: float, n: int) -> float:
-        if x == 0: return 0
-        res = 1
-        if n < 0: x, n = 1 / x, -n
-        while n:
-            if n & 1: res *= x
-            x *= x
-            n >>= 1
-        return res
+    def isBalanced(self, root: TreeNode) -> bool:
+        def isB(root: TreeNode):
+            if not root.left == None and root.right == None :
+                root.val = 1
+            l, r = isB(root.left), isB(root.right)
+            if l - r not in (0,-1,1): return False
+            root. val = max(1+l,1+r)
+        isB(root)
+        return True
 
 sol = Solution()
 print(sol.myPow(2.00000,10))
